116e9503754c35c517dd30abc1584edbd5b6f0d5
yandeu <20306025+yandeu@users.noreply.github.com>
2023-08-28 18:58:04 +0200

    Update to v2.3.2

--- src/server.js
+++ src/server.js
@@ -4,20 +4,14 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 const port = 3000
+const geckos = require('@geckos.io/server/cjs/index').default
+const io = geckos()
 
-const main = async () => {
-  // import geckos as ESM
-  const { geckos } = await import('@geckos.io/server')
-  const io = geckos()
+io.addServer(server)
+io.onConnection(channel => {})
 
-  io.addServer(server)
-  io.onConnection(channel => {})
+app.use(express.static('www'))
 
-  app.use(express.static('www'))
-
-  server.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
-}
-
-main()
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
